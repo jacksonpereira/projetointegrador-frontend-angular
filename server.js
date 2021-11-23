177c1b3f58033c1b0a98a4fe28401fa97f9b2159
Jackson Silva <jacksonga@hotmail.com>
2021-11-22 22:20:03 -0300

    Cofigurando nginx

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const appName = "projetointegrador-frontend-angular"
+const outputPath = `${__dirname}/dist/${appName}`;
+
+app.use(express.static(outputPath));
+app.get('/*', (req,res)=>{
+    res.sendFile(`${outputPath}/index.html`);
+});
+app.listen(proccess.env.PORT);
